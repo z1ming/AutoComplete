7eaa08b4c6a19f9c8c4b0907ab3ee64c4f3c39a6
jamie <mzm@mail.dlue.edu.cn>
2020-10-06 17:40:24 +0800

    refresh allfiles

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Input from './components/Input/input';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Input/>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+library.add(fas)
+
+export { default as AutoComplete } from './components/AutoComplete'
+export { default as Input } from './components/Input'
+export { default as Transition } from './components/Transition'
